b1f04bc650944bab42261048d1d67859d3283d42
AnnaWXM <xiaomininfi@gmail.com>
2023-10-18 14:57:31 +0300

    exe 1.12 anecdotes step1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,43 +1,31 @@
-const Header = () => {
-  return (
-    <h2>
-      <p>Half Stack application development</p>
-    </h2>
-  )
-}
+import { useState } from 'react'
 
-const Content = (props) => {
-  return (
-    <div>
-      <p>{props.name} {props.exercises}</p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Number of exercises: {props.exercises}</p>
-    </div>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  console.log(selected)
 
   return (
     <div>
-      <Header course={course} />
-      <Content name={part1} exercises={exercises1} />
-      <Content name={part2} exercises={exercises2} />
-      <Content name={part3} exercises={exercises3} />
-      <Total exercises={exercises1 + exercises2 + exercises3} />
+      <p>{anecdotes[selected]}</p>
+
+      <Button handleClick={() => setSelected(Math.floor(Math.random() * 8))} text='change anecdotes' />
     </div>
   )
 }

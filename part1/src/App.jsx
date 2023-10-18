00d78ee0678cde435adaf38bb8593bd9e59a6a02
AnnaWXM <xiaomininfi@gmail.com>
2023-10-18 14:34:46 +0300

    exe 1.11 HTML Table

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -8,7 +8,10 @@ const Button = (props) => (
 
 const StatisticLine = (props) => {
   return (
-    <p> {props.text} {props.value}</p>
+    <tr>
+      <td> {props.text}  </td>
+      <td> {props.value} </td>
+    </tr>
   )
 }
 
@@ -20,14 +23,16 @@ const Statistics = (props) => {
     )
   }
   return (
-    <div>
-      <StatisticLine text='good' value={props.good} />
-      <StatisticLine text='neutral' value={props.neutral} />
-      <StatisticLine text='bad' value={props.bad} />
-      <StatisticLine text='all' value={all} />
-      <StatisticLine text='average' value={(props.good - props.bad) / all} />
-      <StatisticLine text='positive' value={props.good / all * 100 + "%"} />
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={props.good} />
+        <StatisticLine text='neutral' value={props.neutral} />
+        <StatisticLine text='bad' value={props.bad} />
+        <StatisticLine text='all' value={all} />
+        <StatisticLine text='average' value={(props.good - props.bad) / all} />
+        <StatisticLine text='positive' value={props.good / all * 100 + "%"} />
+      </tbody>
+    </table>
   )
 }
 

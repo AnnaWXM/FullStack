61fc74fbf3519506d1bf9ab14dc8e6221d323017
AnnaWXM <xiaomininfi@gmail.com>
2023-10-18 14:06:33 +0300

    exe 1.8

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,12 +6,25 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad
+  return (
+    <p>
+      <br />good {props.good}
+      <br />neutral {props.neutral}
+      <br />bad {props.bad}
+      <br />all {all}
+      <br />average {(props.good - props.bad) / all}
+      <br />positive {props.good / all * 100} %
+    </p>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
 
   const setToGood = newValue => {
     console.log('good value now', newValue)
@@ -35,14 +48,7 @@ const App = () => {
       <Button handleClick={() => setToNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setToBad(bad + 1)} text='bad' />
       <h2>statistics</h2>
-      <p>
-        <br />good {good}
-        <br />neutral {neutral}
-        <br />bad {bad}
-        <br />all {all}
-        <br />average {(good - bad) / all}
-        <br />positive {good / all * 100} %
-      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }

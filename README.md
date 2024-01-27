808dcb72e53c37d6e8e93f485fa64ea8267483d6
AnnaWXM <xiaomininfi@gmail.com>
2024-01-27 14:55:50 +0200

    2.5

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FullStack
+
+## environment
+
+set up node: `nvm use 18.18.1`
+
+start the application: `npm run dev`
--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,28 +1,75 @@
-import { useState } from 'react'
-import Note from './components/Note'
+const Header = ({ course }) => <h2>{course.name}</h2>
 
+const Total = ({ parts }) => <b>Number of exercises {parts.reduce((sum, p) => sum + p.exercises, 0)}</b>
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const Part = ({ part }) =>
+  <p>
+    {part.name} {part.exercises}
+  </p>
 
-  const addNote = (event) => {
-    event.preventDefault()
-    console.log('button clicked', event.target)
-  }
+const Content = ({ parts }) =>
+  parts.map(part => (
+    <Part key={part.id} part={part} />
+  ));
+
+const Course = ({ course }) =>
+  <>
+    <Header course={course} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </>
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note =>
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-
-      <form onSubmit={addNote}>
-        <input />
-        <button type="submit">save</button>
-      </form>
+      <h1>Web development curriculum</h1>
+      <Course course={courses[0]} />
+      <Course course={courses[1]} />
     </div>
   )
 }

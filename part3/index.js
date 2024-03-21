b6ce976a0fb5bbd946453a9127a154d493d52e4c
AnnaWXM <xiaomininfi@gmail.com>
2024-03-21 13:52:55 +0200

    3.4

--- part3/index.js
+++ part3/index.js
@@ -32,6 +32,16 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (!person) {
+        response.status(404).json({ Error: 'Not Found' })
+    } else {
+        response.json(person)
+    }
+})
+
 app.get('/info', (request, response) => {
     let len = persons.length
     let currentTime = new Date();

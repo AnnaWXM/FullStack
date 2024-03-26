3812ab50b9a7ebc26a389c9eb70ce51047996d84
AnnaWXM <xiaomininfi@gmail.com>
2024-03-26 21:29:03 +0200

    change to a new repo for deploy the backend online

--- part3/index.js
+++ part3/index.js
@@ -95,6 +95,7 @@ app.get('/info', (request, response) => {
     response.send(message)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
